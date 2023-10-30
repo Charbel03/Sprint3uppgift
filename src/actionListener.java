ef5b4d7af1d03e916a2172edc50787bec8f1ae9d
Charbel Chayeb <charbel.chayeb03@gmail.com>
2023-10-30 15:02:52 +0100

    update

--- src/actionListener.java
+++ src/actionListener.java
@@ -4,11 +4,18 @@ import java.awt.event.ActionListener;
 
 
 public class actionListener implements ActionListener {
-    public actionListener(JButton button) {
+    JButton b1 = new JButton();
+    public actionListener(JButton buttonFromFramePuzzle) {
+        this.b1 = buttonFromFramePuzzle;
+
 
     }
     @Override
     public void actionPerformed(ActionEvent e) {
+
+        if (e.getSource() == b1){
+            System.out.println("hej");
+        }
 
 
     }
